7d406ec37511a6c725c00a99ce126a6db9bc4e19
Jacob Bartel <bartel@cs.unc.edu>
2014-05-19 20:54:07 -0400

    Added names for recipient addressing events

--- src/main/java/metrics/recipients/RecipientAddressingEvent.java
+++ src/main/java/metrics/recipients/RecipientAddressingEvent.java
@@ -2,28 +2,34 @@ package metrics.recipients;
 
 public class RecipientAddressingEvent {
 	
-	private RecipientAddressingEvent() {
-		// Empty;
+	private String name;
+	private RecipientAddressingEvent(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return name;
 	}
 	
 	// Recommendation list generation
-	public static final RecipientAddressingEvent SeedTooSmallForListGeneration = new RecipientAddressingEvent();
-	public static final RecipientAddressingEvent EmptyListGenerated = new RecipientAddressingEvent();
-	public static final RecipientAddressingEvent ListWithNoCorrectEntriesGenerated = new RecipientAddressingEvent();
-	public static final RecipientAddressingEvent ListWithCorrectEntriesGenerated = new RecipientAddressingEvent();
+	public static final RecipientAddressingEvent SeedTooSmallForListGeneration = new RecipientAddressingEvent("SeedTooSmallForListGeneration");
+	public static final RecipientAddressingEvent EmptyListGenerated = new RecipientAddressingEvent("EmptyListGenerated");
+	public static final RecipientAddressingEvent ListWithNoCorrectEntriesGenerated = new RecipientAddressingEvent("ListWithNoCorrectEntriesGenerated");
+	public static final RecipientAddressingEvent ListWithCorrectEntriesGenerated = new RecipientAddressingEvent("ListWithCorrectEntriesGenerated");
 
 	// User actions
-	public static final RecipientAddressingEvent Scan = new RecipientAddressingEvent();
-	public static final RecipientAddressingEvent TypeSingleRecipient = new RecipientAddressingEvent();
-	public static final RecipientAddressingEvent SwitchBetweenClickAndType = new RecipientAddressingEvent();
-	public static final RecipientAddressingEvent AddressingCompleted = new RecipientAddressingEvent();
-	public static final RecipientAddressingEvent SelectSingleRecipient = new RecipientAddressingEvent();
+	public static final RecipientAddressingEvent Scan = new RecipientAddressingEvent("Scan");
+	public static final RecipientAddressingEvent TypeSingleRecipient = new RecipientAddressingEvent("TypeSingleRecipient");
+	public static final RecipientAddressingEvent SwitchBetweenClickAndType = new RecipientAddressingEvent("SwitchBetweenClickAndType");
+	public static final RecipientAddressingEvent AddressingCompleted = new RecipientAddressingEvent("AddressingCompleted");
+	public static final RecipientAddressingEvent SelectSingleRecipient = new RecipientAddressingEvent("SelectSingleRecipient");
 	
 	
 	public static final class SelectMultipleRecipientsEvent extends RecipientAddressingEvent {
 		public final int numRecipients;
 		
 		private SelectMultipleRecipientsEvent(int numRecipients) {
+			super("SelectMultipleRecipientsEvent(" + numRecipients + ")");
 			this.numRecipients = numRecipients;
 		}
 	}

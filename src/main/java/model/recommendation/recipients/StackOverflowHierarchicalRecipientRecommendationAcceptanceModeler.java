ea616a17facfa060efb70bb36b0ee2b00b0db3b5
Jacob Bartel <bartel@cs.unc.edu>
2014-05-22 18:02:17 -0400

    Added hierarchical for stack overflow

---
+++ src/main/java/model/recommendation/recipients/StackOverflowHierarchicalRecipientRecommendationAcceptanceModeler.java
@@ -0,0 +1,20 @@
+package model.recommendation.recipients;
+
+import java.util.Collection;
+
+import metrics.recipients.RecipientMetric;
+import recommendation.recipients.groupbased.hierarchical.HierarchicalRecipientRecommender;
+import data.representation.actionbased.messages.stackoverflow.StackOverflowMessage;
+
+public class StackOverflowHierarchicalRecipientRecommendationAcceptanceModeler<RecipientType extends Comparable<RecipientType>, MessageType extends StackOverflowMessage<RecipientType>>
+		extends
+		NewsgroupHierarchicalRecipientRecommendationAcceptanceModeler<RecipientType, MessageType> {
+	
+	public StackOverflowHierarchicalRecipientRecommendationAcceptanceModeler(int listSize,
+			HierarchicalRecipientRecommender<RecipientType> recommender,
+			Collection<MessageType> trainingMessages,
+			Collection<MessageType> testMessages,
+			Collection<RecipientMetric<RecipientType, MessageType>> metrics) {
+		super(listSize, recommender, trainingMessages, testMessages, metrics);
+	}
+}

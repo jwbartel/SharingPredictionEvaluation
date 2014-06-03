b05a236708ba04ed56437b905a7e4b3ce8d0bfe8
Jacob Bartel <bartel@cs.unc.edu>
2014-06-03 16:14:17 -0400

    Added group dataset summarizer

---
+++ src/main/java/testbed/summarize/GroupDatasetSummarizer.java
@@ -0,0 +1,47 @@
+package testbed.summarize;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import testbed.dataset.group.GroupDataSet;
+import testbed.summarize.SortableColumn.Order;
+
+public class GroupDatasetSummarizer<AccountType> extends
+		DatasetSummarizer<AccountType> {
+	
+	GroupDataSet<AccountType> dataset;
+	
+	private GroupDatasetSummarizer(GroupDataSet<AccountType> dataset) {
+		this.dataset = dataset;
+	}
+
+	@Override
+	public void summarize() throws IOException {
+		if (dataset.getSeedlessMetricsFile().exists()) {
+			summarizeMetricResults(dataset.getSeedlessMetricsFile(), dataset.getAccountIds());
+		}
+		// TOD summarize evolution taking into account multiple tests
+	}
+
+	@Override
+	public GroupedRowSummarizer getGroupRowSummarizer(File resultsFile) {
+		return new GroupedRowSummarizer(resultsFile, "account", "account");
+	}
+
+	@Override
+	public BestColumnsSummarizer getBestColumnsSummarizer(File resultsFile) {
+		if (resultsFile.getName().endsWith(dataset.getSeedlessMetricsFile().getName())) {
+			Collection<SortableColumn> columnsToRankBy = new ArrayList<>();
+			columnsToRankBy.add(new SortableColumn("reqired adds", Order.Ascending));
+			columnsToRankBy.add(new SortableColumn("required deletes", Order.Ascending));
+			if (resultsFile.getName().startsWith("summarized - ")) {
+				return new BestColumnsSummarizer(resultsFile, "type", columnsToRankBy, 1);
+			} else {
+				return new BestColumnsSummarizer(resultsFile, "type", columnsToRankBy);
+			}
+		}
+		return null;
+	}
+}

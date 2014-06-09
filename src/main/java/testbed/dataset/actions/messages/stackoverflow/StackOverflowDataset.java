c92610684c0352f719697b674ba71d59a4234b18
Jacob Bartel <bartel@cs.unc.edu>
2014-06-09 16:27:17 -0400

    Added retrieving of test and train data for response time data

--- src/main/java/testbed/dataset/actions/messages/stackoverflow/StackOverflowDataset.java
+++ src/main/java/testbed/dataset/actions/messages/stackoverflow/StackOverflowDataset.java
@@ -1,11 +1,16 @@
 package testbed.dataset.actions.messages.stackoverflow;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.TreeSet;
+
+import org.apache.commons.io.FileUtils;
 
 import testbed.dataset.actions.messages.MessageDataSet;
 import data.representation.actionbased.messages.stackoverflow.StackOverflowMessage;
@@ -15,10 +20,9 @@ public abstract class StackOverflowDataset<Recipient, MessageType extends StackO
 		extends MessageDataSet<Long, Recipient, MessageType, ThreadType> {
 
 	protected Collection<MessageType> messages = new TreeSet<>();
-	protected Map<Long,ThreadType> threads = new TreeMap<>();
+	protected Map<Long, ThreadType> threads = new TreeMap<>();
 
-	public StackOverflowDataset(String name, Long[] accountIds,
-			File rootFolder) {
+	public StackOverflowDataset(String name, Long[] accountIds, File rootFolder) {
 		super(name, accountIds, rootFolder, Long.class);
 	}
 
@@ -42,21 +46,21 @@ public abstract class StackOverflowDataset<Recipient, MessageType extends StackO
 		}
 		return questions;
 	}
-	
+
 	public Collection<MessageType> getTrainQuestions(Long account, double percentTrain) {
-		
+
 		Collection<MessageType> questions = getQuestions(getAllMessages(account));
 		return getTrainMessages(questions, percentTrain);
 	}
-	
+
 	public Collection<MessageType> getTestQuestions(Long account, double percentTrain) {
-		
+
 		Collection<MessageType> questions = getQuestions(getAllMessages(account));
 		return getTestMessages(questions, percentTrain);
 	}
-	
+
 	public abstract ThreadType createThread();
-	
+
 	protected void addToThread(MessageType message) {
 		Long id = message.getId();
 		ThreadType thread = threads.get(id);
@@ -65,5 +69,79 @@ public abstract class StackOverflowDataset<Recipient, MessageType extends StackO
 			threads.put(id, thread);
 		}
 		thread.addThreadedAction(message);
+	}
+
+	private File getResponseTimesExperimentsFolder() {
+		return new File(getRootFolder(), "response times");
+	}
+
+	private File getResponseTimesFoldsFolder() {
+		return new File(getResponseTimesExperimentsFolder(), "folds");
+	}
+
+	private File getResponseTimesFoldSetsFolder() {
+		return new File(getResponseTimesExperimentsFolder(), "fold sets for experiments");
+	}
+
+	private File getResponseTimesTrainingSetsFile() {
+		return new File(getResponseTimesFoldSetsFolder(), "train_sets.csv");
+	}
+
+	private List<Double> loadTimes(String fold) throws IOException {
+		List<Double> retVal = new ArrayList<>();
+		File foldFile = new File(getResponseTimesFoldsFolder(), fold + "_y.csv");
+		List<String> lines = FileUtils.readLines(foldFile);
+		for (String line : lines) {
+			if (line.equals("Inf")) {
+				retVal.add(Double.POSITIVE_INFINITY);
+			} else {
+				retVal.add(Double.parseDouble(line));
+			}
+		}
+		return retVal;
+	}
+
+	public Map<Integer, List<Double>> getResponesTimesTrainingTimes() throws IOException {
+		Map<Integer, List<Double>> retVal = new TreeMap<>();
+
+		List<String> trainSetsLines = FileUtils.readLines(getResponseTimesTrainingSetsFile());
+		Integer setId = 1;
+		for (String trainSetsLine : trainSetsLines) {
+			String[] trainingSet = trainSetsLine.split(",");
+			List<Double> trainingTimes = new ArrayList<>();
+			for (String trainingFold : trainingSet) {
+				trainingTimes.addAll(loadTimes(trainingFold));
+			}
+			retVal.put(setId, trainingTimes);
+			setId++;
+		}
+
+		return retVal;
+	}
+
+	private File getResponseTimesTestingSetsFile() {
+		return new File(getResponseTimesFoldSetsFolder(), "test_sets.csv");
+	}
+
+	public Map<Integer, List<Double>> getResponesTimesTestingTimes() throws IOException {
+		Map<Integer, List<Double>> retVal = new TreeMap<>();
+
+		List<String> testSetsLines = FileUtils.readLines(getResponseTimesTestingSetsFile());
+		Integer setId = 1;
+		for (String testSetsLine : testSetsLines) {
+			String[] testingSet = testSetsLine.split(",");
+			List<Double> testingTimes = new ArrayList<>();
+			for (String testingFold : testingSet) {
+				testingTimes.addAll(loadTimes(testingFold));
+			}
+			retVal.put(setId, testingTimes);
+			setId++;
+		}
+
+		return retVal;
+	}
+
+	public File getResponseTimesResultsFolder() {
+		return new File(getResponseTimesExperimentsFolder(), "results");
 	}
 }

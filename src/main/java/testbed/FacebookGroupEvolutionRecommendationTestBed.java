c78365729b97d3fcad319f3f610a537e3b00445b
Jacob Bartel <bartel@cs.unc.edu>
2014-08-01 01:34:38 -0400

    Renamed group evolution testbed to be more specific

--- src/main/java/testbed/FacebookGroupEvolutionRecommendationTestBed.java
+++ src/main/java/testbed/FacebookGroupEvolutionRecommendationTestBed.java
@@ -60,7 +60,7 @@ import testbed.dataset.group.GroupDataSet;
 import testbed.dataset.group.MixedInitiativeDataSet;
 import testbed.dataset.group.SnapGroupDataSet;
 
-public class GroupEvolutionRecommendationTestBed {
+public class FacebookGroupEvolutionRecommendationTestBed {
 
 	static double[] growthRates = {0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
 	static int[] testIds = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
